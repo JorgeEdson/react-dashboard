41507f8ab6aa58535693cea248fa874825f909e1
Jorge Edson <jorge.edson.bsi@gmail.com>
2020-02-26 03:13:27 -0300

    Erro no service de resumo

---
+++ src/components/Consultas/Consultas.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import { listarConsultas } from "../../services/service_consultas";
+
+class Consultas extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      realizadas: [],
+      marcadas: []
+    };
+  }
+
+  componentDidMount() {
+    listarConsultas().then(dados => this.setState(dados));
+  }
+
+  render() {
+    return (
+      <div>
+        <h2 className="mt-2">Consultas</h2>
+        <div className="row">
+          <div className="col">
+            <div className="card mt-2">
+              <div className="card-head">Realizadas</div>
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Especialidade</th>
+                    <th>Quantidade</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.state.realizadas.map((item, indice) => {
+                    return (
+                      <tr key={indice}>
+                        <td>{item.especialidade}</td>
+                        <td>{item.quantidade}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card mt-2">
+              <div className="card-head">Marcadas</div>
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Especialidade</th>
+                    <th>Quantidade</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.state.marcadas.map((item, indice) => {
+                    return (
+                      <tr key={indice}>
+                        <td>{item.especialidade}</td>
+                        <td>{item.quantidade}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Consultas;
